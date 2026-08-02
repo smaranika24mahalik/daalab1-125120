041db906ec60ccaab1331f938decee843a24c50d
smaranika24mahalik <b125120@iiit-bh.ac.in>
2026-08-02 23:53:56 +0530

    Update and rename 1.c to lab1/1.c

---
+++ lab1/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Increasing order of growth:\n");
+
+    printf("log2 n\n");
+    printf("sqrt(n)\n");
+    printf("n^0.51\n");
+    printf("n\n");
+    printf("n log2 n\n");
+    printf("100n^2+6n\n");
+    printf("n^2-324\n");
+    printf("2^32 n\n");
+    printf("3^n\n");
+    printf("2n^3\n");
+
+    return 0;
+}
